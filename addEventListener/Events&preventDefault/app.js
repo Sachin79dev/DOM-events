778cd43658ed691b9aee6d645adf9ed0509944ab
Sachin Singh <ss0823903@gmail.com>
2023-10-25 17:27:44 +0530

    Events & PreventDefault Practice
    Creating the form using events and PreventDefault method to add the functionality of adding text into input and getting the list into your unordered list.

---
+++ addEventListener/Events&preventDefault/app.js
@@ -0,0 +1,11 @@
+const form = doument.querySelector("#submitform");
+const input = document.querySelector("#task-list");
+const list = document.querySelector("#tasks");
+form.addEventListener("submit", function(e) {
+    e.preventDefault();
+    const catName = input.value;
+    const newli = document.createelement("li");
+    newli.innerText = catName;
+    list.append(newli);
+    input.value = "";
+});
